e631646adffd87790acccfbb08b4211346d32fb3
Betina Justo <justoritter@gmail.com>
2023-11-21 08:54:52 -0300

    arrumei o multer

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -2,18 +2,12 @@ const multer = require("multer");
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        return cb(null, './src/public');
+        return cb(null, "./src/public");
     },
     filename: function (req, file, cb) {
-        // console.log("teste", file.originalname)
-        if (file.mimetype.startsWith('video/')) {
-            return cb(null, `${Date.now()}_${file.originalname}`);
-        } else {
-            return cb(new Error('O arquivo enviado não é um vídeo.'));
-        }
+        return cb(null, `${Date.now()}_${file.originalname}`);
     }
 });
-
 
 var upload = multer({ storage });
 
